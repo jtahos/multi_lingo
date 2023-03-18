5621c1ca9dc89bbba7095a4fc370bb8134e2e024
jtahos <jtahos@yahoo.com>
2023-03-18 14:58:48 -0400

    created the signals.py file, created the signals function, confiered media files

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db.models.fields.related import ForeignKey, OneToOneField
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -104,3 +106,7 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.user.email
+    
+    
+
+    # post_save.connect(post_save_create_profile_receiver, sender=User)
---
+++ accounts/signals.py
@@ -0,0 +1,26 @@
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from .models import User, UserProfile
+
+
+
+@receiver(post_save, sender=User)
+def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+        print(created)
+        if created:
+            UserProfile.objects.create(user=instance)
+        else:
+            try:
+                profile = UserProfile.objects.get(user=instance)
+                profile.save()
+            except:    
+                # create user profile if it does not exist
+                UserProfile.objects.create(user=instance)
+                print('prfile did not exist, but one was created')
+            print('user is updated successfully')    
+            
+            
+    
+@receiver(pre_save, sender=User)      
+def per_save_profile_receiver(sender, instance, **kwargs):     
+        print(instance.username, 'this new user is being saved') 
