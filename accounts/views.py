c7983153a25d57dd1bfcddcfded7d53db3343dc9
jtahos <jtahos@yahoo.com>
2023-03-25 22:36:36 -0400

    venvor grgestration

--- accounts/views.py
+++ accounts/views.py
@@ -78,3 +78,12 @@ def registerVendor(request):
     }
 
     return render(request, 'accounts/registerVendor.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return
+
+def dashboard(request):
+    return
